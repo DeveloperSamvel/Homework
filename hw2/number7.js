ffadb0b00df90b7d3ed69ffd42320d42271b6770
DeveloperSamvel <developer.vardanyan@gmail.com>
2023-09-02 16:49:33 +0400

    change in number7

--- hw2/number7.js
+++ hw2/number7.js
@@ -1,37 +1,37 @@
-"use strict" 
+"use strict";
 
-const arr = [1, 2, "a", true, {}, undefined, 55, false, "hi", null, 166, NaN, "6"];
-
+const arr = [
+  1,
+  2,
+  "a",
+  true,
+  {},
+  undefined,
+  55,
+  false,
+  "hi",
+  null,
+  166,
+  NaN,
+  "6",
+];
 
 // with method parseInt
-const parseInteger = (users) => {
-    let arr = [];
+const parseInteger = (arr) => {
+  return arr.filter((item) => item === item * 1);
+};
 
-    arr = users.filter(item => item * 1);
-    return arr;
-}
-
-// console.log(parseInteger(arr));
-
-
+console.log(parseInteger(arr));
 
 // with method parseInt
-const parseIntegerVersion2 = (users) => {
-    let arr = [];
-
-    arr = users.filter(item => parseInt(item));
-    return arr;
-}
+const parseIntegerVersion2 = (arr) => {
+  return arr.filter((item) => item === parseInt(item));
+};
 
 // console.log(parseIntegerVersion2(arr));
 
-
-// version where "6" is not the correct answer
-const parseIntegerVersion3 = (users) => {
-    let arr = [];
-
-    arr = users.filter(item => item === Number(item));
-    return arr;
-}
+const parseIntegerVersion3 = (arr) => {
+  return arr.filter((item) => item === Number(item));
+};
 
 // console.log(parseIntegerVersion3(arr));
