842861b36d82e4977b98a9ac896046cc09f1af57
DeveloperSamvel <developer.vardanyan@gmail.com>
2023-10-19 21:11:20 +0400

    changed hw5

--- hw5/binding/index3 call.js
+++ hw5/binding/index3 call.js
@@ -1,10 +1,14 @@
 function myCall(obj, ...props) {
   obj.customCall = this;
-  return obj.customCall(...props);
+  obj.customCall(...props);
+
+  delete obj["customCall"];
 }
 
 function f(age, country) {
-  return `Hello, my name is ${this.name}, I am ${age} years old and I live in ${country}.`;
+  console.log(
+    `Hello, my name is ${this.name}, I am ${age} years old and I live in ${country}.`
+  );
 }
 
 Function.prototype.myCall = myCall;
@@ -13,5 +17,4 @@ const person = {
   name: "Samvel",
 };
 
-let result = f.myCall(person, 26, "Armenia");
-console.log(result);
+f.myCall(person, 26, "Armenia");
