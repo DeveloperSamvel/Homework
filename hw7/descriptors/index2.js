df836ead585efb6b52f3fa8290f69f46de62d055
DeveloperSamvel <developer.vardanyan@gmail.com>
2023-11-02 15:52:22 +0400

    changes hw7

---
+++ hw7/descriptors/index2.js
@@ -0,0 +1,22 @@
+function applyPropertyRules(obj,rules){
+    for(item in obj){
+        Object.defineProperty(obj,item,{
+            writable:rules.writable,
+            enumerable:rules.enumerable,
+            configurable:rules.configurable,
+        })
+        console.log(Object.getOwnPropertyDescriptor(obj,item))    
+    }
+	return obj;
+}
+
+const rule={
+    writable:false,
+    enumerable:false,
+    configurable:false,
+}
+const obj={
+    name:'artak'
+}
+
+applyPropertyRules(obj,rule);
